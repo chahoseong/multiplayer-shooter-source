6810014ea10e94599c46bbd7877d464236ae98f9
Cha Hoseong <hoseong@chahoseong.com>
2025-10-09 16:37:31 +0900

    AimTrace Task

---
+++ MultiplayerShooter/AbilitySystem/Abilities/Tasks/AbilityTask_AimTrace.cpp
@@ -0,0 +1,113 @@
+﻿#include "AbilitySystem/Abilities/Tasks/AbilityTask_AimTrace.h"
+#include "AbilitySystemComponent.h"
+
+UAbilityTask_AimTrace* UAbilityTask_AimTrace::AimTrace(UGameplayAbility* OwningAbility, float MaxRange)
+{
+	UAbilityTask_AimTrace* Task = NewAbilityTask<UAbilityTask_AimTrace>(OwningAbility);
+	Task->MaxRange = MaxRange;
+	return Task;
+}
+
+void UAbilityTask_AimTrace::Activate()
+{
+	if (const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo())
+	{
+		if (ActorInfo->IsLocallyControlled())
+		{
+			SendAimTraceHitResult();
+			return;
+		}
+
+		if (AbilitySystemComponent.IsValid())
+		{
+			FGameplayAbilitySpecHandle AbilitySpecHandle = GetAbilitySpecHandle();
+			FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
+
+			AbilitySystemComponent->AbilityTargetDataSetDelegate(
+				AbilitySpecHandle,
+				ActivationPredictionKey
+			).AddUObject(this, &ThisClass::OnTargetDataReplicatedCallback);
+
+			const bool bIsTargetDataSet = AbilitySystemComponent->CallReplicatedTargetDataDelegatesIfSet(
+				AbilitySpecHandle,
+				ActivationPredictionKey
+			);
+			if (!bIsTargetDataSet)
+			{
+				SetWaitingOnRemotePlayerData();
+			}
+		}
+	}
+}
+
+void UAbilityTask_AimTrace::SendAimTraceHitResult() const
+{
+	FScopedPredictionWindow ScopedPredictionWindow(AbilitySystemComponent.Get());
+
+	APlayerController* PlayerController =
+		Ability->GetCurrentActorInfo()->PlayerController.Get();
+	check(PlayerController);
+
+	FVector ViewStart;
+	FRotator ViewRotation;
+	PlayerController->GetPlayerViewPoint(ViewStart, ViewRotation);
+	const FVector ViewDirection = ViewRotation.Vector();
+	FVector ViewEnd = ViewStart + ViewDirection * MaxRange;
+	
+	UWorld* World = PlayerController->GetWorld();
+	FCollisionQueryParams QueryParams(
+		SCENE_QUERY_STAT(AimTrace_SendAimTraceHitResult),
+		false,
+		PlayerController->GetPawn()
+	);
+	
+	FHitResult Hit;
+	World->LineTraceSingleByChannel(
+		Hit,
+		ViewStart,
+		ViewEnd,
+		ECC_Visibility,
+		QueryParams
+	);
+
+	if (!Hit.bBlockingHit)
+	{
+		Hit.ImpactPoint = ViewEnd;
+	}
+
+	FGameplayAbilityTargetDataHandle TargetDataHandle;
+	TargetDataHandle.Add(new FGameplayAbilityTargetData_SingleTargetHit(Hit));
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->ServerSetReplicatedTargetData(
+			GetAbilitySpecHandle(),
+			GetActivationPredictionKey(),
+			TargetDataHandle,
+			FGameplayTag(),
+			AbilitySystemComponent->ScopedPredictionKey
+		);
+	}
+	
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(TargetDataHandle);
+	}
+}
+
+void UAbilityTask_AimTrace::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& Data,
+	FGameplayTag ActivationTag)
+{
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->ConsumeClientReplicatedTargetData(
+			GetAbilitySpecHandle(),
+			GetActivationPredictionKey()
+		);
+	}
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(Data);
+	}
+}
---
+++ MultiplayerShooter/AbilitySystem/Abilities/Tasks/AbilityTask_AimTrace.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_AimTrace.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAimTraceHit, const FGameplayAbilityTargetDataHandle&, Data);
+
+UCLASS()
+class MULTIPLAYERSHOOTER_API UAbilityTask_AimTrace : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="Ability|Tasks", meta=(HidePine="OwningAbility", DefaultToSelf="OwningAbility", BlueprintInternalUseOnly="True"))
+	static UAbilityTask_AimTrace* AimTrace(UGameplayAbility* OwningAbility, float MaxRange = 999999.0f);
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnAimTraceHit ValidData;
+	
+private:
+	virtual void Activate() override;
+
+	void SendAimTraceHitResult() const;
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& Data, FGameplayTag ActivationTag);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	float MaxRange;
+};
