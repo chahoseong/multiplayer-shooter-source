f20c993b47288748a0d4ebe40d0c657fe22bfe0e
Cha Hoseong <hoseong@chahoseong.com>
2025-10-09 16:38:24 +0900

    Add Projectile

---
+++ MultiplayerShooter/Weapons/MultiplayerShooterProjectile.cpp
@@ -0,0 +1,11 @@
+﻿#include "Weapons/MultiplayerShooterProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AMultiplayerShooterProjectile::AMultiplayerShooterProjectile(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+}
---
+++ MultiplayerShooter/Weapons/MultiplayerShooterProjectile.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "GameFramework/Actor.h"
+#include "MultiplayerShooterProjectile.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class MULTIPLAYERSHOOTER_API AMultiplayerShooterProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMultiplayerShooterProjectile(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+};
