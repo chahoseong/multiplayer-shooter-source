8b5d87af7fa5633fbace99f4b54b6807d0c13e00
Cha Hoseong <hoseong@chahoseong.com>
2025-12-13 19:23:41 +0900

    PlayerController

---
+++ Dust/Player/DustPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DustPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUST_API ADustPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
