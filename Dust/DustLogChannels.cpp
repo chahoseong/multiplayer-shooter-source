d1a7339afaebd7fa8e12e0ed13b2cebe6455cee6
Cha Hoseong <hoseong@chahoseong.com>
2025-12-16 23:06:49 +0900

    Add dust log category

---
+++ Dust/DustLogChannels.cpp
@@ -0,0 +1,34 @@
+﻿#include "DustLogChannels.h"
+
+DEFINE_LOG_CATEGORY(LogDust);
+
+FString GetClientServerContextString(UObject* ContextObject)
+{
+	ENetRole Role = ROLE_None;
+	
+	if (const AActor* Actor = Cast<AActor>(ContextObject))
+	{
+		Role = Actor->GetLocalRole();
+	}
+	else if (const UActorComponent* Component = Cast<UActorComponent>(ContextObject))
+	{
+		Role = Component->GetOwnerRole();
+	}
+	
+	if (Role != ROLE_None)
+	{
+		return Role == ROLE_Authority ? TEXT("Server") : TEXT("Client");
+	}
+	else
+	{
+#if WITH_EDITOR
+		if (GIsEditor)
+		{
+			extern ENGINE_API FString GPlayInEditorContextString;
+			return GPlayInEditorContextString;
+		}
+#endif
+	}
+	
+	return TEXT("[]");
+}
---
+++ Dust/DustLogChannels.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "Logging/LogMacros.h"
+
+DUST_API DECLARE_LOG_CATEGORY_EXTERN(LogDust, Log, All);
+
+DUST_API FString GetClientServerContextString(UObject* ContextObject = nullptr);
