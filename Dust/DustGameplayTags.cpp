3cd472afbba40d24ac59743ee2ba20aceaffa221
Cha Hoseong <hoseong@chahoseong.com>
2025-12-16 23:07:03 +0900

    Define input tags

---
+++ Dust/DustGameplayTags.cpp
@@ -0,0 +1,8 @@
+﻿#include "DustGameplayTags.h"
+
+namespace DustGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Move, "Input.Action.Move")
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Look, "Input.Action.Look")
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Jump, "Input.Action.Jump")
+}
---
+++ Dust/DustGameplayTags.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace DustGameplayTags
+{
+	DUST_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Action_Move)
+	DUST_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Action_Look)
+	DUST_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Input_Action_Jump)
+}
