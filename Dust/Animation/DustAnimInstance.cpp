7419e4cbf56e37f45e498bdedf50764a1fdb0124
Cha Hoseong <hoseong@chahoseong.com>
2025-12-24 11:53:20 +0900

    Add gameplay tag property map to anim instance

---
+++ Dust/Animation/DustAnimInstance.cpp
@@ -0,0 +1,51 @@
+﻿#include "Animation/DustAnimInstance.h"
+#include "AbilitySystemGlobals.h"
+#include "Expected.h"
+#include "GameFramework/Character.h"
+#include "Misc/DataValidation.h"
+
+UDustAnimInstance::UDustAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UDustAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* AbilitySystem)
+{
+	check(AbilitySystem);
+	
+	GameplayTagPropertyMap.Initialize(this, AbilitySystem);
+}
+
+void UDustAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	Expect(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(GetOwningActor()))
+	.AndThen([this](UAbilitySystemComponent* AbilitySystemComponent){
+		InitializeWithAbilitySystem(AbilitySystemComponent);
+	});
+}
+
+void UDustAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	// Expect(Cast<ACharacter>(GetOwningActor()), IsValid, TEXT(""))
+	// .AndThen([](const ACharacter* Character)
+	// {
+	// 	
+	// });
+}
+
+#if WITH_EDITOR
+EDataValidationResult UDustAnimInstance::IsDataValid(FDataValidationContext& Context) const
+{
+	Super::IsDataValid(Context);
+	
+	GameplayTagPropertyMap.IsDataValid(this, Context);
+	
+	return Context.GetNumErrors() > 0 ? EDataValidationResult::Invalid : EDataValidationResult::Valid;
+}
+#endif
+
+
---
+++ Dust/Animation/DustAnimInstance.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "GameplayEffectTypes.h"
+#include "DustAnimInstance.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS(Config=Game)
+class DUST_API UDustAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UDustAnimInstance(const FObjectInitializer& ObjectInitializer);
+	
+	virtual void InitializeWithAbilitySystem(UAbilitySystemComponent* AbilitySystem);
+	
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) const override;
+#endif
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Character State Data")
+	float GroundDistance = -1.0f;
+};
