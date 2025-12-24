abacc89840894f513109cfd9f8f9547174f60463
Cha Hoseong <hoseong@chahoseong.com>
2025-12-24 11:53:32 +0900

    DustPlayerController

--- Dust/Player/DustPlayerController.cpp
+++ Dust/Player/DustPlayerController.cpp
@@ -1,4 +1,1 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "DustPlayerController.h"
+﻿#include "Player/DustPlayerController.h"
--- Dust/Player/DustPlayerController.h
+++ Dust/Player/DustPlayerController.h
@@ -1,14 +1,9 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "DustPlayerController.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class DUST_API ADustPlayerController : public APlayerController
 {
